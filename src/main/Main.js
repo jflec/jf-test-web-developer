21f9a5465bfea4ecde2e82038db9ec0d865a357b
JFlec <josephflec@gmail.com>
2022-01-24 16:39:06 -0600

    desktop

--- src/main/Main.js
+++ src/main/Main.js
@@ -19,12 +19,7 @@ const Main = () => {
           euismod lacinia. Diam maecenas sed enim ut sem viverra aliquet eget
           sit. Enim sed faucibus turpis in eu mi bibendum. Convallis aenean et
           tortor at risus viverra adipiscing at in. Velit ut tortor pretium
-          viverra. Proin fermentum leo vel orci porta non pulvinar neque. A
-          iaculis at erat pellentesque adipiscing commodo elit at imperdiet.
-          Cursus turpis massa tincidunt dui. Ac auctor augue mauris augue neque
-          gravida in fermentum et. Ac orci phasellus egestas tellus. Neque
-          volutpat ac tincidunt vitae semper quis lectus nulla. Integer enim
-          neque volutpat ac tincidunt vitae semper quis. A erat nam at lectus.
+          viverra.
         </p>
         <p>
           Morbi tincidunt augue interdum velit euismod in. Imperdiet proin
@@ -32,16 +27,7 @@ const Main = () => {
           integer quis auctor elit sed vulputate mi. Id velit ut tortor pretium.
           Molestie ac feugiat sed lectus vestibulum. Sodales ut eu sem integer
           vitae. Odio tempor orci dapibus ultrices. Rhoncus aenean vel elit
-          scelerisque mauris pellentesque. Ornare quam viverra orci sagittis.
-          Risus at ultrices mi tempus imperdiet nulla. Enim nunc faucibus a
-          pellentesque. Adipiscing commodo elit at imperdiet dui accumsan sit
-          amet nulla. Scelerisque eleifend donec pretium vulputate sapien.
-          Adipiscing bibendum est ultricies integer quis auctor elit sed
-          vulputate. In vitae turpis massa sed elementum tempus egestas sed.
-          Auctor augue mauris augue neque gravida. Massa enim nec dui nunc
-          mattis enim ut. Amet massa vitae tortor condimentum lacinia quis vel
-          eros. Amet luctus venenatis lectus magna fringilla urna porttitor
-          rhoncus. Pulvinar etiam non quam lacus.
+          scelerisque mauris pellentesque.
         </p>
         <p>
           At urna condimentum mattis pellentesque. Arcu dictum varius duis at
@@ -49,10 +35,8 @@ const Main = () => {
           convallis posuere morbi leo urna. Enim nec dui nunc mattis enim ut.
           Lorem mollis aliquam ut porttitor leo a diam. Nunc sed blandit libero
           volutpat sed cras ornare. Sodales ut etiam sit amet nisl purus in
-          mollis. Elementum facilisis leo vel fringilla est ullamcorper.
-          Maecenas pharetra convallis posuere morbi leo urna molestie at
-          elementum. Aliquam purus sit amet luctus venenatis lectus magna
-          fringilla urna.
+          mollis. Ornare quam viverra orci sagittis. Risus at ultrices mi tempus
+          imperdiet nulla. Enim nunc faucibus a pellentesque.
         </p>
       </div>
       <div id="image-object-container">
